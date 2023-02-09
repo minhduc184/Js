e37b469e417b66a41783d1ffe300cca0ed108876
minhduc184 <nmhwww3@gmail.com>
2023-02-09 15:55:55 +0700

    add

---
+++ assets/js/style.js
@@ -0,0 +1,303 @@
+// //cap nhat thay doi thuong xuyen
+// let name = "Nguyen Van A";
+// var age = "19";
+
+// //cap nhat thay doi it 
+// const email = "nguyenvana@gmail.com"; 
+
+// console.log(name);
+// console.log(age);
+// console.log(email);
+
+// let city;
+// city = "Ha Noi";
+// console.log(city);
+
+// let City;
+// City = "Ho Chi Minh";
+// console.log(City);
+
+//kiem tra kieu du lieu cua bien
+// let number = 10;
+
+// console.log(typeof(number));
+
+// let email = "nguyenvana@gmail.com"
+// console.log(typeof(email));
+
+// const age = "100";
+
+// function hello(){
+//     let name = "hello javascript base";
+//     console.log(name);
+//     // console.log(age);
+// }
+
+
+// hello();
+// // hello();
+// hello();
+// hello();
+
+// {
+//     let name = "Javascipt base";
+//     console.log(name);
+// }
+
+// console.log(name);
+
+// let name = 'my name\'s Dung';
+// console.log(name);
+
+// let name = "xin chao cac ban." +
+// " toi ten la Minh Duc."+
+// " toi o Le Van Luong";
+// console.log(name);
+
+// const name = "Minh Duc" 
+// const rich = "Rat la giau";
+
+// const namerich= name + " " + rich;
+// console.log(namerich);
+
+// const name = "Minh Duc";
+// const age = 30;
+// const address = "43 Le van luong"
+
+// const fullname = `toi ten la ${name},
+// nam nay toi ${age} 
+// dia chi nha toi o ${address} `;
+// console.log(fullname)
+
+///////////////////////////////////////////////////////
+
+
+// 1. Hop le
+// const $usd = 10000;
+// console.log($usd);
+
+// 2.Hop le
+// const test123 = 'test';
+// console.log(test123);
+
+// 3.Hop le
+// const username = 'minh duc';
+// console.log(username);
+
+// 4.Khong hop le vi chua dau -
+// const docker7890- = 'docker';
+// console.log(docker7890-);
+
+// 5.Hop le
+// const _self = 'ban than';
+// console.log(_self);
+
+// 6.Khong hop le vi bat dau bang so
+// const 1a = 'ban than';
+// console.log(1a);
+
+// 7.Hop le
+// const string = 30;
+// console.log(string);
+
+//8. Hop le
+// const first_name = 'Nguyen';
+// console.log(first_name);
+
+//9. Khong hop le vi dat ten trung tu khoa Javascript
+// const let= 'Nguyen';
+// console.log(let);
+
+//10. Hop le
+// const variable = 'Nguyen';
+// console.log(variable);
+
+//11. Hop le
+// const Object = 'Nguyen';
+// console.log(Object);
+
+//12. Khong hop le vi chua dau -
+// const my-name = 'Nguyen';
+// console.log(my-name);
+
+//13. Khong hop le vi chua dau '
+// const can't = 'Nguyen';
+// console.log(can't);
+
+//14. Hop le
+// const error = 'bi loi';
+// console.log(error);
+
+//15. Hop le
+// const Person = 'Con Nguoi';
+// console.log(Person);
+
+//16. Hop le
+// const numBer = 123;
+// console.log(numBer);
+
+
+
+
+//Buoi 2
+
+// function hello(number) {
+//     console.log(`hello 500 anh em $(number)` );
+//     console.log(number);
+// }
+
+// hello(12);
+// hello(15);
+
+
+
+
+// function sum(a,b) {
+    // let a = 5
+    // // let b = 5
+    // let resultSum = a + b
+//     return resultSum;
+// }
+
+// const resultSum = sum(5,6);
+// console.log(resultSum);
+// console.log(sum(5,6));
+
+// let a = 10;
+// if (a < 20) {
+//     console.log("a nho hon 10");
+// }else{
+//     console.log("a lonn hon 10");
+// }
+  
+// let a = 100;
+// if(a == 50) {
+//     console.log("a bang 50");
+// }else if(a == 100) {
+//     console.log("a bang 100");
+// }else if(a == 500){
+//     console.log("a bang 500");
+// }else{
+//     console.log("a bang 1000");
+// }
+
+// let price = 10000;
+
+// switch (price) {
+//     case 12000:
+//         console.log("Ca phe sua") ;
+//         break;
+
+//     case 10000:
+//         console.log("Ca phe da") ;
+//         break;
+
+//     case 8000:
+//         console.log("Sting dau") ;
+//         break;
+        
+//     case 2000:
+//         console.log("Tra da") ;
+//         break;
+
+//     default:
+//         console.log("Khong tim thay do uong");
+//         break;
+// }
+
+//Array
+// let nameArr = ["Thanh","Huy","Khanh"];
+// const age =[]
+
+// const royal = ["nhieu tien", "gioi lap trinh", "dep trai", "duyem dang"];
+// console.log(royal[1]);
+// console.log(royal[2]);
+
+// royal[0]="rat ngheo";
+// console.log(royal[0]);
+
+// const a = 1;
+// const b = a + 1;
+// console.log(a);
+// console.log(b);
+
+// let x = [1];
+// let y = x;
+// console.log(x);
+// console.log(y);
+
+// let arrArr = ["image_1", "image_2", "image_3"]
+
+// console.log[arrArr.length]; //do dai mang 3
+
+// for(let i = 0; i < arrArr.length; i++) {
+//     console.log(arrArr[i], "Lay gia tri phan tu cua mang");
+//     console.log(i, "Gia tri cua i qua moi vong lap");
+// }
+
+
+
+
+
+
+
+
+
+                        //Btap ve nha
+// Bai 1
+// let mark = 75;
+// if (mark >= 85) {
+//     console.log("A");
+// }else if (70 <= mark && mark < 85) {
+//     console.log("B");
+// }else if (40 <= mark && mark <70) {
+//     console.log("C");
+// } else {
+//  console.log("D");
+// }
+
+// Bai 2
+// let a = 5;
+// let b = 10;
+// if (a < b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+//Bai 3
+// let a = 1;
+// if (a < 0) {
+//     console.log("a la so am ");
+// } else if (a > 0) {
+//     console.log("a la so duong");
+// } else {
+//     console.log("a la so 0");
+// }
+
+//Bai 4
+// let a = 6;
+// if ( a % 2 == 0) {
+//     console.log("a la so chan");
+// } else {
+//     console.log("a la so le");
+// }
+
+//Bai 5
+// let a = 5;
+// if ( a % 3 == 0 && a % 5 == 0) {
+//     console.log("a chia het cho 3 va 5");
+// } else {
+//     console.log("a khong chia het cho 3 va 5");
+// }
+
+//Bai 6
+// let a = 1;
+// let b = 5;
+// const tong = a + b;
+// let c = 6;
+// if (c == tong) {
+//     console.log("c co bang a + b");
+// } else {
+//     console.log("c khong bang a + b");
+// }
